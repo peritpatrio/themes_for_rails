638a9183afa0fab38522e8ef0f5789520ac71431
Petri Partio <petri.partio@gmail.com>
2016-02-20 11:18:46 +0200

    Add support for Rails 4

--- lib/themes_for_rails/routes.rb
+++ lib/themes_for_rails/routes.rb
@@ -4,14 +4,14 @@ module ThemesForRails
 
     def themes_for_rails
       theme_dir = ThemesForRails.config.themes_routes_dir
-      constraints = { :theme => /[\w\.]*/ } 
-      
-      match "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', 
-        :as => :base_theme_stylesheet, :constraints => constraints
-      match "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', 
-        :as => :base_theme_javascript, :constraints => constraints
-      match "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', 
-        :as => :base_theme_image, :constraints => constraints
+      constraints = { theme: /[\w\.]*/ }
+
+      get "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets',
+          as: :base_theme_stylesheet, constraints: constraints
+      get "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts',
+          as: :base_theme_javascript, constraints: constraints
+      get "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images',
+          as: :base_theme_image, constraints: constraints
     end
 
   end
@@ -24,4 +24,3 @@ module ActionDispatch::Routing
 
   end
 end
-
